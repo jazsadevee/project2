9fd55e2e8c1b7b224130b1b758d96a0d7dd4b4df
JazSade Villian <jazsadevee@gmail.com>
2023-02-21 17:09:05 -0500

    create api receips

--- download.js
+++ download.js
@@ -1,7 +1,7 @@
-import fetch from 'node-fetch'
-import { promises as fsPromises } from 'fs'
+// import fetch from 'node-fetch'
+// import { promises as fsPromises } from 'fs'
 
 
-fetch('https://www.themealdb.com/api/json/v1/1/lookup.php?i=52772')
-  .then(response => response.json())
-  .then(data => fsPromises.writeFile('./data.json', JSON.stringify(data)))
+// fetch('https://www.googleapis.com/calendar/v3/calendars/calendarId')
+//   .then(response => response.json())
+//   .then(data => fsPromises.writeFile('./data.json', JSON.stringify(data)))
