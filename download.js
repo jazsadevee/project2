85f55c428967ccc8cef1711a914044227ada0094
JazSade Villian <jazsadevee@gmail.com>
2023-02-21 15:50:03 -0500

    fetch data from API complete

--- download.js
+++ download.js
@@ -1,6 +1,7 @@
 import fetch from 'node-fetch'
-// import { promises as fsPromises } from 'fs'
+import { promises as fsPromises } from 'fs'
 
-fetch('https://www.themealdb.com/api')
+
+fetch('https://www.themealdb.com/api/json/v1/1/lookup.php?i=52772')
   .then(response => response.json())
-  .then(data => console.log(data))
+  .then(data => fsPromises.writeFile('./data.json', JSON.stringify(data)))
