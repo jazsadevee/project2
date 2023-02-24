d22ae605e3ee2cf1256fd1242ab7f514b8df2752
JazSade Villian <jazsadevee@gmail.com>
2023-02-24 11:17:54 -0500

    fixed routing & deleted connections.js

--- Seed/seed.js
+++ Seed/seed.js
@@ -1,20 +1,28 @@
-import db from "../db/connections.js";
+import 'dotenv/config'
 import Meals from "./Meals.json" assert { type: "json" }
 // import Ingredient from "../models/ingredient.js" 
 import Recipe1 from "../models/recipe.js"
+import mongoose from 'mongoose';
 
+console.log(process.env.DATABASE_URL);
 
 const insertData = async () => {
-  // await db.dropDatabase();
-  
+
   await Recipe1.deleteMany();
   await Recipe1.insertMany(Meals);
 
   // await Ingredient.create(Ingredient);
 
-  await db.close();
 };
+mongoose.connect(process.env.DATABASE_URL)
+mongoose.set('strictQuery', true)
 
-insertData();
+await seed()
+await insertData();
+await mongoose.disconnect()
 
 
+
+async function seed() {
+
+}
