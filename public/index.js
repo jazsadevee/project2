d6268ba95c9e20d43d36eb3571a775e85cbf8e8b
JazSade Villian <jazsadevee@gmail.com>
2023-02-28 08:15:23 -0500

    commented out frontend

--- public/index.js
+++ public/index.js
@@ -3,36 +3,36 @@
 // import recipe from "../models/recipe";
 
 // let mealContainer = document.getElementsByClassName('meals-list')
-let mealHolder = document.getElementById('recipe')
-let mealsList = document.querySelector('.meals-list')
-console.log(mealHolder);
+// let mealHolder = document.getElementById('recipe')
+// let mealsList = document.querySelector('.meals-list')
+// console.log(mealHolder);
 
-async function fetchData() {
-  const baseUrl = `https://project2-sooty.vercel.app/api`
-  try {
-    const res = await fetch(`${baseUrl}`)
-    const json = await res.json()
-    console.log('fetch');
-    console.log(json);
-    json.map(meal => {
-      mealHolder.textContent = meal.meal
-      let mealName = document.createElement("p")
-      mealName.textContent = meal.meal
-      mealsList.appendChild(mealName)
-      console.log(mealName);
+// async function fetchData() {
+//   const baseUrl = `https://project2-sooty.vercel.app/api`
+//   try {
+//     const res = await fetch(`${baseUrl}`)
+//     const json = await res.json()
+//     console.log('fetch');
+//     console.log(json);
+//     json.map(meal => {
+//       mealHolder.textContent = meal.meal
+//       let mealName = document.createElement("p")
+//       mealName.textContent = meal.meal
+//       mealsList.appendChild(mealName)
+//       console.log(mealName);
       // if (meal.category === 'Chicken') {
       //   console.log(true, meal)
       // } else {
       //   console.log(false);
       // }
       // console.log(meal.category);
-    })
-    return json
-  }
-  catch {
-    console.log("error");
-  }
-}
-fetchData()
+//     })
+//     return json
+//   }
+//   catch {
+//     console.log("error");
+//   }
+// }
+// fetchData()
 
 
